8b5a78da7e22d31088caa21c4eba9e0b852d6f2a
saurabh.dhillon <saurabh.dhillon@delhivery.com>
2019-12-09 15:53:30 +0530

    [#OS-665] added promise date

--- app/src/main/java/com/delhivery/axle/data/home/trips/HomeTripsItemData.kt
+++ app/src/main/java/com/delhivery/axle/data/home/trips/HomeTripsItemData.kt
@@ -2,6 +2,7 @@ package com.delhivery.axle.data.home.trips
 
 import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
+import com.delhivery.axle.R
 import com.delhivery.axle.api.response.BulkPaymentItem
 import com.delhivery.axle.data.BaseKeyTypeModel
 import com.delhivery.axle.data.fuelcards.FuelCardData
@@ -10,13 +11,14 @@ import com.delhivery.axle.ui.bids.TripType.AdvancePending
 import com.delhivery.axle.ui.bids.TripType.BalancePending
 import com.delhivery.axle.ui.bids.TripType.Completed
 import com.delhivery.axle.utils.ColorProviderUtils
-import com.delhivery.axle.utils.DatePatterns
+import com.delhivery.axle.utils.DatePatterns.OrionDateFormat
 import com.delhivery.axle.utils.DateUtils
 import com.delhivery.axle.utils.DrawableProviderUtils
 import com.delhivery.axle.utils.StringUtils
 import com.delhivery.axle.utils.extensions.isNotNullOrEmpty
 import com.google.gson.annotations.SerializedName
 import java.io.Serializable
+import java.util.Calendar
 
 data class HomeTripsItemData(
   @SerializedName("LR") val lr: String,
@@ -40,6 +42,7 @@ data class HomeTripsItemData(
   @SerializedName("payment_mode") val paymentMode: String? = null,
   @SerializedName("truck_display_name") val truckDisplayName: String? = "",
   @SerializedName("pod_url") val podUrl: String? = "",
+  @SerializedName("promise_date") val promiseDate: String? = "",
   var payment: BulkPaymentItem? = null,
   var fuelCard: FuelCardData? = null
 ) : BaseKeyTypeModel<String>(), Serializable {
@@ -114,17 +117,26 @@ data class HomeTripsItemData(
   }
 
   /**
+   * @return promise date
+   */
+  fun promiseDate() = promiseDate?.let {
+    "PD: " + DateUtils.formatDate(
+        DateUtils.parseDate(it, OrionDateFormat), "dd-MMM-yyyy"
+    )
+  }
+
+  /**
    * Formatted required at
    */
   fun requiredAt() =
-    DateUtils.formatDate(DateUtils.parseDate(displayTime(), DatePatterns.OrionDateFormat), "dd MMM")
+    DateUtils.formatDate(DateUtils.parseDate(displayTime(), OrionDateFormat), "dd MMM")
 
   /**
    * Required at background as per designs
    */
   @DrawableRes
   fun requiredAtBg() =
-    DrawableProviderUtils.daysDiffBgDrawableRes(displayTime(), DatePatterns.OrionDateFormat)
+    DrawableProviderUtils.daysDiffBgDrawableRes(displayTime(), OrionDateFormat)
 
   /**
    * Required at text color as per status
@@ -132,6 +144,19 @@ data class HomeTripsItemData(
   @ColorRes
   fun requiredTextColor() =
     ColorProviderUtils.getTripStatusColor(tripStatus().typeText.toLowerCase())
+
+  /**
+   * Required at text color as per promise date
+   */
+  @ColorRes
+  fun requiredPromiseDateColor() = promiseDate?.let {
+    val calendar = Calendar.getInstance()
+    calendar.set(Calendar.HOUR, calendar.getActualMinimum(Calendar.HOUR))
+    calendar.set(Calendar.MINUTE, calendar.getActualMinimum(Calendar.MINUTE))
+    ColorProviderUtils.getPromiseDateColor(
+        (calendar.timeInMillis - DateUtils.parseDate(it, OrionDateFormat).time)
+    )
+  } ?: R.color.sub_heading_black
 
   /**
    * Check if fuel balance is available or not
