b3354fcb0bc5e9dc0ad04984d0260e179bc6fbed
Vibhor <vibhor.srivastava@delhivery.com>
2020-09-15 12:59:00 +0530

    OS-2392 added additional field for 100 km logic

--- app/src/main/java/com/delhivery/axle/api/service/TransactionService.kt
+++ app/src/main/java/com/delhivery/axle/api/service/TransactionService.kt
@@ -24,7 +24,8 @@ interface TransactionService {
     @Query("origin_city_code") source: String? = null,
     @Query("destination_city_code") destination: String? = null,
     @Query("truck_type") truckType: String? = null,
-    @Query("axle_current_week_loads") currWeekLoads: String = "yes"
+    @Query("axle_current_week_loads") currWeekLoads: String = "yes",
+    @Query("apply_100km_logic") nearby100kmcities: Boolean = true
   ): Single<BaseResponse<TransactionsResponse>>
 
   /**
