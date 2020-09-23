addc657fd30a56c408fafe5e5219f6289b18e69f
Vibhor <vibhor.srivastava@delhivery.com>
2020-09-23 16:06:00 +0530

    OS-2345 lowest price while placing bid fix

--- app/src/main/java/com/delhivery/axle/data/home/bids/HomeBidsRequestItemData.kt
+++ app/src/main/java/com/delhivery/axle/data/home/bids/HomeBidsRequestItemData.kt
@@ -286,7 +286,7 @@ data class HomeBidsRequestItemData(
   /**
    * @return lowest bid difference
    */
-  fun lowestbidDifference() = if (transactionBid?.bidAmount ?: 0.0 > lowestBid ?: 0.0) {
+  fun lowestbidDifference() = if ((transactionBid?.bidAmount ?: 0.0 > lowestBid ?: 0.0) && (lowestBid ?: 0.0 > 0.0)) {
     " (${StringUtils.formatAmount((transactionBid?.bidAmount ?: 0.0) - (lowestBid ?: 0.0))}"
   } else {
     ""
@@ -295,7 +295,7 @@ data class HomeBidsRequestItemData(
   /**
    * @return set image if supplier bid is more than lowest bid
    */
-  fun setLowestBidImage() = if (transactionBid?.bidAmount ?: 0.0 > lowestBid ?: 0.0) {
+  fun setLowestBidImage() = if ((transactionBid?.bidAmount ?: 0.0 > lowestBid ?: 0.0) && (lowestBid ?: 0.0 > 0.0)) {
     View.VISIBLE
   } else {
     View.GONE
@@ -304,7 +304,7 @@ data class HomeBidsRequestItemData(
   /**
    * @return set close bracket text if lowest bid is present
    */
-  fun setCloseText() = if (transactionBid?.bidAmount ?: 0.0 > lowestBid ?: 0.0) {
+  fun setCloseText() = if ((transactionBid?.bidAmount ?: 0.0 > lowestBid ?: 0.0) && (lowestBid ?: 0.0 > 0.0)) {
     View.VISIBLE
   } else {
     View.GONE
