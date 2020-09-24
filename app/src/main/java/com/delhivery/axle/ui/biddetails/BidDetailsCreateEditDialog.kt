2b5f11cddaa112f91a44d93e0739685ff4dec75e
Vibhor <vibhor.srivastava@delhivery.com>
2020-09-24 11:54:24 +0530

    OS-2345 bid update price in pmt trips

--- app/src/main/java/com/delhivery/axle/ui/biddetails/BidDetailsCreateEditDialog.kt
+++ app/src/main/java/com/delhivery/axle/ui/biddetails/BidDetailsCreateEditDialog.kt
@@ -103,6 +103,10 @@ class BidDetailsCreateEditDialog @Inject constructor(
               }
               amount = (input * transaction.requestedCapacityMg).toInt()
               binding.labelBid.text = "Your minimum payout will be ₹ $amount"
+
+              if (abs((input * transaction.requestedCapacityMg) - (transactionBid?.pmtRate ?: 0.0)) < 500) {
+                throw Exception( "*Bid difference should be more that ₹500")
+              }
             } else {
               amount = input
             }
