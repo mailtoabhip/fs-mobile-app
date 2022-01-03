a0783bfc63bfe568c3872a3f6b700c86b59c6e7d
AakashAgrawalDelhivery <aakash.agarwal@delhivery.com>
2022-01-03 10:00:45 +0530

    AB#143188-Add-Inventory

--- app/src/main/java/com/delhivery/axle/ui/home/fragments/trucks/HomeTrucksRVAdapter.kt
+++ app/src/main/java/com/delhivery/axle/ui/home/fragments/trucks/HomeTrucksRVAdapter.kt
@@ -1,4 +1,79 @@
 package com.delhivery.axle.ui.home.fragments.trucks
 
-class HomeTrucksRVAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.ViewDataBinding
+import com.delhivery.axle.databinding.*
+import com.delhivery.axle.ui.base.BaseViewHolder
+import com.delhivery.axle.ui.base.adapter.BaseDataRVAdapter
+import com.delhivery.axle.ui.base.adapter.DataRVAdapterOperationType
+import com.delhivery.axle.ui.home.fragments.trucks.HomeTrucksRVAdapterItemType
+
+class HomeTrucksRVAdapter(private val _interface : HomeTrucksRVAdapterInterface):
+    BaseDataRVAdapter<BaseHomeTrucksRVAdapterItem<*>, ViewDataBinding, BaseViewHolder<*>>(
+        _interface
+) {
+
+    override fun getItemViewType(position: Int) = items[position].type.typeId
+
+    override fun getBinding(
+        inflater: LayoutInflater,
+        parent: ViewGroup,
+        viewType: Int
+    ) = when (HomeTrucksRVAdapterItemType.byTypeId(viewType)) {
+        HomeTrucksRVAdapterItemType.Search -> ViewHomeTrucksSearchItemBinding.inflate(inflater, parent, false)
+        HomeTrucksRVAdapterItemType.Progress -> ViewHomeTrucksProgressItemBinding.inflate(inflater, parent, false)
+        HomeTrucksRVAdapterItemType.Warning -> ViewWarningItemBinding.inflate(inflater, parent, false)
+        HomeTrucksRVAdapterItemType.Info -> ViewHomeTrucksInfoItemBinding.inflate(inflater, parent, false)
+        HomeTrucksRVAdapterItemType.Timeout -> ViewTimeOutItemBinding.inflate(inflater, parent, false)
+        HomeTrucksRVAdapterItemType.Filters -> ViewHomeTrucksFilterItemBinding.inflate(inflater, parent, false)
+        else -> ViewHomeTrucksRequestItemBinding.inflate(inflater, parent, false)
+    }
+
+    override fun createVH(binding: ViewDataBinding) = when(binding) {
+        is ViewHomeTrucksSearchItemBinding -> HomeTrucksSearchItemVH(binding)
+        is ViewHomeTrucksProgressItemBinding -> HomeTrucksProgressItemVH(binding)
+        is ViewWarningItemBinding -> HomeTrucksWarningItemVH(binding)
+        is ViewHomeTrucksInfoItemBinding -> HomeTrucksInfoItemVH(binding)
+        is ViewTimeOutItemBinding -> HomeTrucksTimeOutItemVH(binding)
+        is ViewHomeTrucksFilterItemBinding -> HomeTrucksFilterItemVH(binding)
+        else -> HomeTrucksRequestItemVH(binding as ViewHomeTrucksRequestItemBinding)
+    }
+    
+
+    override fun bindVH(
+        holder: BaseViewHolder<*>,
+        item: BaseHomeTrucksRVAdapterItem<*>) {
+        when(holder) {
+            is HomeTrucksRequestItemVH -> holder.bind(item as HomeTrucksRequestItem, _interface)
+            is HomeTrucksProgressItemVH -> holder.bind(item as HomeTrucksProgressItem, _interface)
+            is HomeTrucksSearchItemVH -> holder.bind(item as HomeTrucksSearchItem, _interface)
+            is HomeTrucksWarningItemVH -> holder.bind(item as HomeTrucksWarningItem, _interface)
+            is HomeTrucksTimeOutItemVH -> holder.bind(item as HomeTrucksTimeoutItem, _interface)
+            is HomeTrucksInfoItemVH -> holder.bind(item as HomeTrucksInfoItem, _interface)
+            is HomeTrucksFilterItemVH -> holder.bind(item as HomeTrucksFilterItem, _interface)
+        }
+
+    }
+
+    /**
+     * Reset all data, remove all errors/transactions
+     */
+    fun resetStaticData() {
+        mutableListOf<Pair<BaseHomeTrucksRVAdapterItem<*>, DataRVAdapterOperationType>>().apply {
+            add(Pair(HomeTrucksProgressItem(), DataRVAdapterOperationType.AddUpdate))
+            items.filter {
+                it.type == HomeTrucksRVAdapterItemType.Request || it.type == HomeTrucksRVAdapterItemType.Warning ||
+                it.type == HomeTrucksRVAdapterItemType.Timeout || it.type == HomeTrucksRVAdapterItemType.Info || it.type == HomeTrucksRVAdapterItemType.MoreInfo
+                || it.type == HomeTrucksRVAdapterItemType.Search || it.type == HomeTrucksRVAdapterItemType.Filters
+            }
+                .map { Pair(it, DataRVAdapterOperationType.Remove) }
+                .let {
+                    addAll(it)
+                }
+        }
+            .let {
+                operation(it)
+            }
+    }
 }
