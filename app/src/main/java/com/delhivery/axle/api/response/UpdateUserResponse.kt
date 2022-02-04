934f546528d39b24585565872d04a751524dccb1
deeshantrajput <deeshant.rajput@delhivery.com>
2022-02-04 14:20:34 +0530

    AB#148992-otp-and-account-set-up-changes

--- app/src/main/java/com/delhivery/axle/api/response/UpdateUserResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.delhivery.axle.api.response
-
-import com.google.gson.annotations.SerializedName
-
-
-data class UpdateUserResponse(
-        @SerializedName("success") val successMsg: String
-)
