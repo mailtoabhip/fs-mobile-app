fab0ecd5f5a7c2b52bba89369af8e53e9cc59342
ashish01aryan <ashish.aryan@delhivery.com>
2022-04-20 13:22:47 +0530

    AB#156608 (payment page)

--- app/src/main/java/com/delhivery/axle/ui/profile/MyProfileActivity.kt
+++ app/src/main/java/com/delhivery/axle/ui/profile/MyProfileActivity.kt
@@ -5,7 +5,6 @@ import android.graphics.Color
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.os.Environment
-import android.util.Log
 import android.view.View
 import android.view.ViewTreeObserver
 import androidx.appcompat.widget.AppCompatImageView
