7fb4807872a6aa162d4ae761497013821b3f2aff
saurabh dhillion <saurabh.dhillon@ergodisruption.com>
2022-04-28 15:25:58 +0530

    AB#157043 rejection policy

--- app/src/main/java/com/delhivery/axle/ui/paymentdetails/VendorPolicyActivity.kt
+++ app/src/main/java/com/delhivery/axle/ui/paymentdetails/VendorPolicyActivity.kt
@@ -14,55 +14,55 @@ import javax.inject.Inject
 
 class VendorPolicyActivity : BaseActivity<ActivityVendorPolicyBinding, PaymentDetailsViewModel>() {
 
-       @Inject
-       lateinit var userPrefs: UserPrefs
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            binding.webView.webViewClient = WebViewClient()
-          binding.webView.settings.javaScriptEnabled = true
+    @Inject
+    lateinit var userPrefs: UserPrefs
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        binding.webView.webViewClient = WebViewClient()
+//        binding.webView.settings.javaScriptEnabled = true
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+//        binding.webView.settings.setSupportZoom(true)
+        setSupportActionBar(binding.toolbar)
+        title = ""
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+        binding.vendorPolicyLink.setOnClickListener {
+            val browserIntent =
+                Intent(Intent.ACTION_VIEW, Uri.parse("https://orion.delhivery.com/#/paymentterms"))
+            startActivity(browserIntent)
         }
+//        val pdf = "https://orion.delhivery.com/assets/orion_vendor_policy.pdf"
+//        binding.webView.loadUrl("https://docs.google.com/gview?embedded=true&url=$pdf")
 
-  override fun onPostCreate(savedInstanceState: Bundle?) {
-    super.onPostCreate(savedInstanceState)
-    binding.webView.settings.setSupportZoom(true)
-    setSupportActionBar(binding.toolbar)
-    title = ""
-    supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-
-    binding.vendorPolicyLink.setOnClickListener {
-      val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://orion.delhivery.com/#/paymentterms"))
-      startActivity(browserIntent)
-    }
-    val pdf = "https://orion.delhivery.com/assets/orion_vendor_policy.pdf"
-    binding.webView.loadUrl("https://docs.google.com/gview?embedded=true&url=$pdf")
-
-    viewModel.vendorUserUpdateLiveData.observe(this, Observer {
-      if(it) {
-        userPrefs.vendorPolicyAccepted = true
-        navigationUtils.showKycSubmittedDialog()
-      }
-    })
-    binding.buttonIAgree.setOnClickListener{
-      viewModel.updateUserDetailsForVendorPolicy()
-    }
-
-  }
-
-
-
-        override fun onBackPressed() {
-            if (webView.canGoBack())
-                webView.goBack()
-            else {
-              super.onBackPressed()
-              navigationUtils.navigate(PaymentDetailsActivity::class.java,true)
+        viewModel.vendorUserUpdateLiveData.observe(this, Observer {
+            if (it) {
+                userPrefs.vendorPolicyAccepted = true
+                navigationUtils.showKycSubmittedDialog()
             }
+        })
+        binding.buttonIAgree.setOnClickListener {
+            viewModel.updateUserDetailsForVendorPolicy()
         }
+
+    }
+
+
+    override fun onBackPressed() {
+        if (webView.canGoBack())
+            webView.goBack()
+        else {
+            super.onBackPressed()
+            navigationUtils.navigate(PaymentDetailsActivity::class.java, true)
+        }
+    }
 
     override fun getViewModelClass() = PaymentDetailsViewModel::class.java
 
-    override fun layoutId()= R.layout.activity_vendor_policy
+    override fun layoutId() = R.layout.activity_vendor_policy
 
-    override fun requireConnection()=true
+    override fun requireConnection() = true
 }
