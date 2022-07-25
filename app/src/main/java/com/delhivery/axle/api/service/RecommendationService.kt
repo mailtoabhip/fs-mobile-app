2149de904e9c1d9d7cd309d5bea26a057c726473
rkcybero <rahul.kumar01@delhivery.com>
2022-07-25 16:22:40 +0530

    AB#169321

--- app/src/main/java/com/delhivery/axle/api/service/RecommendationService.kt
+++ app/src/main/java/com/delhivery/axle/api/service/RecommendationService.kt
@@ -22,14 +22,5 @@ interface RecommendationService {
     @POST("/get_sp_loads")
     fun recommendationTransactions(
       @Body request: ReccomdationRequest
-//            @Query("sp_id") userId: String,
-//            @Query("offset") offset: Int,
-//            @Query("limit") limit: Int
-//            @Query("demand_types") vendorType: String ? = "orion",
-//            @Query("truck_types") vehicleType: String? = null,
-//            @Query("valid_loads_only") validLoads: String = "yes",
-//            @Query("exclude_truck_types") excludeTruckTypes: String? = null,
-//            @Query("filter_vehicle_type") filterVehicleType: Boolean?= null,
-//            @Query("bidding_going_on") biddingGoingOn: Boolean?= false
     ): Single<BaseResponse<TransactionsResponse>>
 }
