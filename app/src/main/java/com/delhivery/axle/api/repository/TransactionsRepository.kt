7872f874383bec9266ea85ceedb5a2f633331eb0
rkcybero <rahul.kumar01@delhivery.com>
2023-01-30 14:50:55 +0530

    AB#188335 removed demand type fixes

--- app/src/main/java/com/delhivery/axle/api/repository/TransactionsRepository.kt
+++ app/src/main/java/com/delhivery/axle/api/repository/TransactionsRepository.kt
@@ -35,8 +35,9 @@ class TransactionsRepository @Inject constructor(
    */
   fun fetchRecommTransactions(offset: Int, demand_type: String, vehicle_type: String?= null,excludeTruckTypes: String?= null, filterVehicleType: Boolean?= null, biddingGoingOn:Boolean = false) =
          recommendationService.recommendationTransactions(
-                  ReccomdationRequest( userRepository.userId(),UserTripsLoadLimit,offset,
-             demand_type, vehicle_type)
+                  ReccomdationRequest( userRepository.userId(),UserTripsLoadLimit,offset)
+//             demand_type, vehicle_type,
+//                  "yes", excludeTruckTypes, filterVehicleType, biddingGoingOn
           ).convertResponse()
 
   fun fetchContractsTransactions(offset: Int, demand_type: String) =
