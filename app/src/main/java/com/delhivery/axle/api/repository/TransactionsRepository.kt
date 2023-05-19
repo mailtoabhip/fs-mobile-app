c4bba4a37bfec65c3df343fb9ee1362032c348d7
Shashank Munda <shashankmunda.int@delhivery.com>
2023-05-19 12:52:38 +0530

    AB#205209 100kms logic applied on contracts search

--- app/src/main/java/com/delhivery/axle/api/repository/TransactionsRepository.kt
+++ app/src/main/java/com/delhivery/axle/api/repository/TransactionsRepository.kt
@@ -74,7 +74,7 @@ class TransactionsRepository @Inject constructor(
     contractType:String?
   ) = transactionService.transactions(
       offset, if(contractType!=ContractType.INTRACITY.type) Requested.statusId + "," + InEnquiry.statusId else null, source, destination, truckType, truckDisplayName,
-    contractsMap[contractStatus]?.statusId, if(requestType=="load") "yes" else null,if (requestType=="load") true else null, if(requestType=="load") "fixed,spot" else "contract",
+    contractsMap[contractStatus]?.statusId, if(requestType=="load") "yes" else null,true, if(requestType=="load") "fixed,spot" else "contract",
     contractType,if(requestType=="load") null else true
   ).convertResponse()
 
