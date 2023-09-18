6d71bfce2ae61f7b8524a3c4780c252cebd2d064
bassicshashank <shashank.munda@delhivery.com>
2023-09-18 17:41:52 +0530

    AB#219294 handle refresh token case for SDK 31 and above

--- app/src/main/java/com/delhivery/axle/tokenExpiryHandling/RefreshTokenWorker.kt
+++ app/src/main/java/com/delhivery/axle/tokenExpiryHandling/RefreshTokenWorker.kt
@@ -7,10 +7,29 @@ import android.content.Intent
 import android.os.Build
 import android.util.Log
 import androidx.work.CoroutineWorker
+import androidx.work.ListenableWorker
+import androidx.work.WorkManager
 import androidx.work.WorkerParameters
+import com.delhivery.axle.BuildConfig
+import com.delhivery.axle.SyncOfferData.MyWorker
+import com.delhivery.axle.api.repository.LoadCycleRepository
+import com.delhivery.axle.api.repository.PriceRepository
+import com.delhivery.axle.config.UrlConfig.AppID
+import com.delhivery.axle.database.AppDatabase
+import com.delhivery.axle.injection.module.DaggerWorkerFactory
+import com.delhivery.axle.utils.extensions.isNotNullOrEmpty
+import com.delhivery.axle.utils.prefs.UserPrefs
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import org.json.JSONObject
 import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
 
-class RefreshTokenWorker(appContext: Context, params: WorkerParameters) :
+class RefreshTokenWorker(appContext: Context, params: WorkerParameters,var userPrefs: UserPrefs, var okHttpClient: OkHttpClient) :
     CoroutineWorker(appContext, params) {
     companion object {
         const val WORK_NAME = "RefreshTokenWorker"
@@ -21,7 +40,9 @@ class RefreshTokenWorker(appContext: Context, params: WorkerParameters) :
             Log.d("prefs","Work Started")
             if(!isMyServiceRunning(RefreshAuthTokenService::class.java)) {
                 val intent = Intent(applicationContext, RefreshAuthTokenService::class.java)
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
+                  validateAndRefreshToken()
+                else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                     applicationContext.startForegroundService(intent)
                 } else {
                     applicationContext.startService(intent)
@@ -42,5 +63,54 @@ class RefreshTokenWorker(appContext: Context, params: WorkerParameters) :
         return false
     }
 
+  private fun validateAndRefreshToken() {
+    Log.d("prefs","service")
 
+    if (userPrefs.jwtToken != null) {
+      Log.d("prefs","service2")
+      if (BuildConfig.FLAVOR == "development" || BuildConfig.FLAVOR == "uat") {
+        refreshToken("https://api-stage-ums.delhivery.com/v2/refresh_token/?force=1")
+      } else {
+        refreshToken("https://api-ums.delhivery.com/v2/refresh_token/?force=1")
+      }
+    }
+  }
+
+  private fun refreshToken(url: String) {
+    val request = Request.Builder()
+      .url(url)
+      .addHeader("X-APP-ID", AppID.url())
+      .build()
+
+    okHttpClient.newCall(request).enqueue(object : Callback {
+      override fun onFailure(call: Call, e: IOException) {
+
+      }
+
+      override fun onResponse(call: Call, response: Response) {
+        try {
+          val strResponse = response?.body()?.string()
+          val json = JSONObject(strResponse)
+          if (!json.isNull("jwt")) {
+            val jwtToken = json.optString("jwt")
+            if (jwtToken.isNotNullOrEmpty()){
+              userPrefs.jwtToken = jwtToken
+            }
+          }
+          WorkManager.getInstance(applicationContext).cancelUniqueWork(RefreshTokenWorker.WORK_NAME)
+        } catch (e: Exception){
+          e.printStackTrace()
+        }
+      }
+    })
+  }
+
+  class Factory @Inject constructor(
+    val userPrefs: UserPrefs,
+    val okHttpClient: OkHttpClient
+  ) : DaggerWorkerFactory.ChildWorkerFactory {
+
+    override fun create(appContext: Context, params: WorkerParameters): ListenableWorker =
+      RefreshTokenWorker(appContext, params,userPrefs, okHttpClient)
+  }
 }
